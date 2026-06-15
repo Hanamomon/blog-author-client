8e308150d503c603fbd4db96b1fb670759cae431
Hanamomon <145081281+Hanamomon@users.noreply.github.com>
2026-06-15 15:15:44 -0700

    feat: add login validation using react hook form

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,17 +1,68 @@
-import { Form } from 'react-router';
+import { useForm } from 'react-hook-form';
+import { useSubmit } from 'react-router';
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submit = useSubmit();
+
   return (
-    <Form method="POST" action="/">
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input name="username" id="username" type="text" />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input name="password" id="password" type="password" />
-      </div>
-      <button>Login</button>
-    </Form>
+    <main>
+      {Object.keys(errors).length > 0 && (
+        <ul>
+          {errors?.username && <li>{errors.username.message}</li>}
+          {errors?.password && <li>{errors.password.message}</li>}
+        </ul>
+      )}
+      <form
+        onSubmit={handleSubmit((data) => {
+          submit(data, { action: '/', method: 'POST' });
+        })}
+      >
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            {...register('username', {
+              required: 'Please enter a username.',
+              pattern: {
+                value: /^[\w\d]+$/gm,
+                message: 'Username should only contain letters and numbers.',
+              },
+              maxLength: {
+                value: 30,
+                message: 'Username cannot exceed 30 characters.',
+              },
+            })}
+            id="username"
+            type="text"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            {...register('password', {
+              required: 'Please enter a password.',
+              minLength: {
+                value: 8,
+                message: 'Password should contain at least 8 characters.',
+              },
+              pattern: {
+                value:
+                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[-#!$@£%^&*()_+|~=`{}[\]:";'<>?,./\\ ]).{1,30}$/gm,
+                message:
+                  'Password should contain at least one lowercase character, one uppercase character, one digit and one symbol.',
+              },
+            })}
+            id="password"
+            type="password"
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </main>
   );
 }
