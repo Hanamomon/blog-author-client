c2a6f6ad13a8635de720782610b4c3ef053f280d
Hanamomon <145081281+Hanamomon@users.noreply.github.com>
2026-08-18 09:40:42 -0700

    fix: unify error boundary's error structure

--- src/Error/ErrorPage.jsx
+++ src/Error/ErrorPage.jsx
@@ -1,13 +1,27 @@
-import { useRouteError } from 'react-router';
+import { useRouteError, isRouteErrorResponse } from 'react-router';
 
 function ErrorPage() {
   const error = useRouteError();
 
-  return (
-    <main>
-      <p>{error.error.message}</p>
-    </main>
-  );
+  if (isRouteErrorResponse(error)) {
+    console.log(error);
+
+    return (
+      <>
+        <h1>
+          {error.status} {error.statusText}
+        </h1>
+        <p>{error.data}</p>
+      </>
+    );
+  } else if (error instanceof Error) {
+    <div>
+      <h1>Error:</h1>
+      <p>{error.message}</p>
+    </div>;
+  } else {
+    return <h1>Unexpected Error</h1>;
+  }
 }
 
 export default ErrorPage;
