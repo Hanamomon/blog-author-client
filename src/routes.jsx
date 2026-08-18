f0e02df77f4f2a0417f370f7cbbcf1814808c1f8
Hanamomon <145081281+Hanamomon@users.noreply.github.com>
2026-08-18 09:45:40 -0700

    feat: route post entry component

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,14 @@
 import App from '@/App/App';
 import Posts from '@/Posts/Posts';
 import PostList from '@/Posts/PostList';
+import PostEntry from '@/Posts/PostEntry';
 import loginAction from '@/Login/login-action';
 import { authLoader, rootLoader } from '@/Login/auth-loader';
 import logoutAction from '@/Login/logout-action';
 import postLoader from '@/Posts/post-loader';
+import postEntryLoader from '@/Posts/post-entry-loader';
 import setPublishAction from '@/Posts/set-publish-action';
+import postEntryAction from '@/Posts/post-entry-action';
 import ErrorPage from '@/Error/ErrorPage';
 
 const routes = [
